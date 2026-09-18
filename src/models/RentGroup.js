65f21777880143fbea079946b7fbba8332326765
nfor-donavan <nfordonavan21@gmail.com>
2026-09-18 12:57:57 +0100

    fix rent group member/utility ids

--- src/models/RentGroup.js
+++ src/models/RentGroup.js
@@ -2,31 +2,46 @@ const mongoose = require("mongoose");
 
 const memberSchema = new mongoose.Schema(
   {
+    id: { type: String, required: true },
     name: { type: String, required: true },
     moveInDay: { type: Number, default: 1 },
   },
-  { _id: false }
+  { _id: false },
 );
 
 const utilitySchema = new mongoose.Schema(
   {
+    id: { type: String, required: true },
     name: { type: String, required: true },
     amount: { type: Number, default: 0 },
   },
-  { _id: false }
+  { _id: false },
 );
 
 const rentGroupSchema = new mongoose.Schema(
   {
-    organization: { type: mongoose.Schema.Types.ObjectId, ref: "Organization", required: true, index: true },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    propertyId: { type: mongoose.Schema.Types.ObjectId, ref: "Property", required: true },
+    organization: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Organization",
+      required: true,
+      index: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    propertyId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Property",
+      required: true,
+    },
     name: { type: String, required: true },
     totalRent: { type: Number, default: 0 },
     members: [memberSchema],
     utilities: [utilitySchema],
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 module.exports = mongoose.model("RentGroup", rentGroupSchema);
